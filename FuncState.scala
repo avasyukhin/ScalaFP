cc6692290ad52cc487c787b89aada9a90ab9d2cb
vasyukhinas <avasyukhin@altarix.ru>
2017-12-07 23:07:17 +0400

    add nonNegativeInt

--- FuncState.scala
+++ FuncState.scala
@@ -8,4 +8,15 @@ case class SimpleRNG(seed:Long) extends RNG{
     val n = (newSeed >>> 16).toInt
     (n,nextRNG)
   }
+  def nonNegativeInt:(Int,SimpleRNG) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    val nPos = n match {
+      case Int.MinValue => Int.MaxValue
+      case k if (k<0) => -1*k
+      case k => k
+      }
+    (nPos,nextRNG)
+    }
 }
