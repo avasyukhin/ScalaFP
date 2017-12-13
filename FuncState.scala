b5ac0f7445bbc4cfb70f5042075ef6243a5692c0
vasyukhinas <avasyukhin@altarix.ru>
2017-12-13 21:55:19 +0400

    ad maps

--- FuncState.scala
+++ FuncState.scala
@@ -2,6 +2,14 @@ trait RNG{
   def nextInt: (Int,RNG)
 }
 case class SimpleRNG(seed:Long) extends RNG{
+type Rand[+A] = RNG => (A, RNG)
+def map[A,B](s: Rand[A])(f: A => B): Rand[B]=
+  rng => {
+    val (a, rng2) = s(rng)
+    (f(a), rng2)
+  }
+  def unit[A](a:A):Rand[A]=
+    rng => (a,rng)
   def nextInt:(Int,RNG) = {
     val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
     val nextRNG = SimpleRNG(newSeed)
@@ -18,5 +26,36 @@ case class SimpleRNG(seed:Long) extends RNG{
       case k => k
       }
     (nPos,nextRNG)
-    }
+  }         
+  def double(rng: RNG):(Double,RNG) = {
+    val (int,nextRNG) = rng.nextInt
+    val d = int match{
+      case k if ((k==Int.MinValue)||(k==Int.MaxValue)) => Int.MaxValue-1
+      case k if (k<0) => -1*k
+      case k => k
+    } 
+    (d.toDouble/Int.MaxValue,nextRNG)
+  }
+ def doubleFromMap:Rand[Double]=
+   map(_.nextInt)(i => {val d = i match{
+      case k if ((k==Int.MinValue)||(k==Int.MaxValue)) => Int.MaxValue-1
+      case k if (k<0) => -1*k
+      case k => k
+   }
+   d.toDouble/Int.MaxValue}
+   )
+ def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = 
+   rng =>{
+     val (a,rng1) = ra(rng)
+     val (b,rng2) = rb(rng1)
+     (f(a,b),rng2)
+   }
+ def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+   fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
+
+ def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+   rng =>{
+    val (a,rng1) = f(rng)
+    g(a)(rng1)
+   }
 }
